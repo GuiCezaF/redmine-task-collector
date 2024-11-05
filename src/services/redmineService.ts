23493fa4f1251dd41630cd13f3332c6d7af18a5c
Guilherme.Felipe <guilherme.felipe@retta.com.br>
2024-11-05 08:27:05 -0300

    feat: criando as interfaces para tipar o servico do redmine

--- src/services/redmineService.ts
+++ src/services/redmineService.ts
@@ -1,7 +1,6 @@
 import { env } from '../config/env';
+import { Issue, RedmineIssuesResponse } from '../types/redmineTypes';
 import Logger from '../utils/logger';
-
-// TODO(Guilherme): Tipar o retorno do handleFetchTasks, fazer uma interface com o retorno esperado
 
 class RedmineService {
   private logger = new Logger();
@@ -9,7 +8,8 @@ class RedmineService {
   private REDMINE_MY_ID = env.REDMINE_USER_ID;
   private REDMINE_URL = env.REDMINE_URL;
 
-  public async handleFetchTasks(): Promise<any> {
+
+  public async handleFetchTasks(): Promise<Issue[] | null> {
     const url = `${this.REDMINE_URL}/issues.json?assigned_to_id=${this.REDMINE_MY_ID}`;
 
     try {
@@ -28,15 +28,16 @@ class RedmineService {
         return null;
       }
 
-      //TODO(Guilherme): Tipar o data para um array das tasks
-      const data = await response.json();
+      const data: RedmineIssuesResponse = (await response.json()) as RedmineIssuesResponse;
       this.logger.info('Tasks fetched successfully');
+
       return data.issues;
     } catch (error) {
       this.logger.error('Error while fetching tasks', error);
       return null;
     }
   }
+
 }
 
 export default RedmineService;
---
+++ src/types/redmineTypes.ts
@@ -0,0 +1,50 @@
+export interface RedmineIssuesResponse {
+  issues: Issue[];
+  total_count: number;
+  offset: number;
+  limit: number;
+}
+
+export interface Issue {
+  id: number;
+  project: Project;
+  tracker: Tracker;
+  status: Status;
+  priority: Priority;
+  author: User;
+  assigned_to?: User;
+  subject: string;
+  description?: string; 
+  start_date?: string; 
+  due_date?: string; 
+  done_ratio: number;
+  estimated_hours?: number;
+  created_on: string; 
+  updated_on: string; 
+  closed_on?: string | null;
+}
+
+export interface Project {
+  id: number;
+  name: string;
+}
+
+export interface Tracker {
+  id: number;
+  name: string;
+}
+
+export interface Status {
+  id: number;
+  name: string;
+}
+
+export interface Priority {
+  id: number;
+  name: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+}
